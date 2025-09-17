196dc55bac0077686dff65c239b9069bc8a941f8
luthfibintang <azisya.luthfibintang@gmail.com>
2025-09-17 21:27:00 +0700

    fix: correct import case for Homepage component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Homepage from './pages/homepage.jsx'
+import Homepage from './pages/Homepage.jsx'
 import PortfolioPage from "./pages/PortfolioPage.jsx"
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router'
